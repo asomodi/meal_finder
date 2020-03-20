f0188b83100227801e210db35bb1c82967a221df
Akos Somodi <akossomodi@yahoo.com>
2020-03-20 15:35:25 +0100

    fetch meal by ID

--- script.js
+++ script.js
@@ -47,5 +47,33 @@ function searchMeal(e) {
   }
 }
 
+// Fetch meal by ID
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsElement.addEventListener('click', e => {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  console.log(mealInfo);
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealByID(mealID);
+  }
+});
